3aa5bed3e4bb9e13de53c158dc02690d76658fa0
H G <47661750+HaileyGu@users.noreply.github.com>
2020-05-01 19:12:41 +0200

    Toggling Todos

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TodoListItem } from './TodoListItem';
 
-const todos: Todo[] = [
+const initialTodos: Todo[] = [
   {
     text: 'Walk the dog',
     complete: false,
@@ -13,10 +13,25 @@ const todos: Todo[] = [
 ];
 
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map(todo => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
   return (
     <ul>
-      <TodoListItem todo={todos[0]} />
-      <TodoListItem todo={todos[1]} />
+      <TodoListItem todo={todos[0]} toggleTodo={toggleTodo} />
+      <TodoListItem todo={todos[1]} toggleTodo={toggleTodo} />
     </ul>
   );
 }
--- todo-list/src/types.d.ts
+++ todo-list/src/types.d.ts
@@ -2,3 +2,5 @@ interface Todo {
   text: string;
   complete: boolean;
 }
+
+type ToggleTodo = (selectedTodo: Todo) => void;
