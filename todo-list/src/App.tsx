8fd68a26ae216885dd19ccc473c3d49a8e32dd28
H G <47661750+HaileyGu@users.noreply.github.com>
2020-05-01 19:13:49 +0200

    Creating a TodoList Component

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TodoListItem } from './TodoListItem';
+import { TodoList } from './TodoList';
 
 const initialTodos: Todo[] = [
   {
@@ -28,12 +28,7 @@ function App() {
     setTodos(newTodos);
   };
 
-  return (
-    <ul>
-      <TodoListItem todo={todos[0]} toggleTodo={toggleTodo} />
-      <TodoListItem todo={todos[1]} toggleTodo={toggleTodo} />
-    </ul>
-  );
+  return <TodoList todos={todos} toggleTodo={toggleTodo} />;
 }
 
 export default App;
