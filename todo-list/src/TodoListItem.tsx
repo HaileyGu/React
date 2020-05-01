7f1c2a0f69f99db705c0905cd8017f8bfaefe630
H G <47661750+HaileyGu@users.noreply.github.com>
2020-05-01 18:57:14 +0200

    Define Todo and Props

--- todo-list/src/TodoListItem.tsx
+++ todo-list/src/TodoListItem.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
-export const TodoListItem = () => {
+interface Todo {
+  text: string;
+  complete: boolean;
+}
+
+interface Props {
+  todo: Todo;
+}
+
+export const TodoListItem: React.FC<Props> = props => {
   return <li>content</li>;
 };
