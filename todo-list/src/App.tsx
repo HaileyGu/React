aaec2c50862027663eb5249f39bef60926434b8a
H G <47661750+HaileyGu@users.noreply.github.com>
2020-05-01 19:06:49 +0200

    Including The TodoListItem in App

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { TodoListItem } from './TodoListItem';
+
+const todos: Todo[] = [
+  {
+    text: 'Walk the dog',
+    complete: false,
+  },
+  {
+    text: 'Write app',
+    complete: true,
+  },
+];
 
 function App() {
   return (
-    <>Hello World</>
+    <ul>
+      <TodoListItem todo={todos[0]} />
+      <TodoListItem todo={todos[1]} />
+    </ul>
   );
 }
 
